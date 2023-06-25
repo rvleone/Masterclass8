0232529963b456a74977264723d68c2f9be20e2c
rvleone <rvleone@gmail.com>
2023-06-25 17:55:47 -0300

    mario_2d

---
+++ dart_playground/mario_2d/main.dart
@@ -0,0 +1,29 @@
+import 'dart:math';
+
+import 'personagem.dart';
+
+void main() {
+  Personagem personagem = Personagem();
+  int numPersonagem = 0;
+  String nomePersonagem;
+  for (int i = 0; i <= 3;) {
+    numPersonagem = Random().nextInt(4) + 1;
+    if (numPersonagem == 1) {
+      nomePersonagem = personagem.personagem1;
+      print('O player ${i + 1} está com o persaonagem $nomePersonagem');
+    }
+    if (numPersonagem == 2) {
+      nomePersonagem = personagem.personagem2;
+      print('O player ${i + 1} está com o persaonagem $nomePersonagem');
+    }
+    if (numPersonagem == 3) {
+      nomePersonagem = personagem.personagem3;
+      print('O player ${i + 1} está com o persaonagem $nomePersonagem');
+    }
+    if (numPersonagem == 4) {
+      nomePersonagem = personagem.personagem3;
+      print('O player ${i + 1} está com o persaonagem $nomePersonagem');
+    }
+    i++;
+  }
+}
---
+++ dart_playground/mario_2d/personagem.dart
@@ -0,0 +1,11 @@
+class Personagem {
+  final personagem1 = 'Mario';
+  final personagem2 = 'Luigi';
+  final personagem3 = 'Peach';
+  final personagem4 = 'Deise';
+
+  get mario => personagem1;
+  get luigi => personagem2;
+  get peach => personagem3;
+  get deise => personagem4;
+}
